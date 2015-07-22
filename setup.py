1fa78a09cc9ab8281856b013285008597422dd82
Casey Law <caseyjlaw@gmail.com>
2015-07-22 08:11:38 -0600

    added dependency

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     author_email = 'caseyjlaw@gmail.com',
     version = '0.0',
     packages = find_packages(),        # get all python scripts in realtime
+    dependency_links = ['http://github.com/caseyjlaw/rtpipe', 'http://github.com/caseyjlaw/sdmpy', 'http://github.com/caseyjlaw/sdmreader'],
     scripts = ['choose_SDM_scans.pl', 'rqmanage.sh'],   # add non-python scripts
     py_modules=['mcaf_monitor', 'queue_monitor', 'queue_rtpipe', 'rqinfo_monitor', 'rqsettings_aoc', 'rqsettings_cbe'],    # set up for click
     install_requires=[
