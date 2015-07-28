e1d907ca8c883a6f154cbf3449fc6eb0d5779d5b
Casey Law <caseyjlaw@gmail.com>
2015-07-27 18:42:24 -0600

    working click mcaf monitor in

--- realfast/scripts/mcaf_monitor.py
+++ realfast/scripts/mcaf_monitor.py
@@ -14,51 +14,23 @@ import logging
 import asyncore
 import subprocess
 import realfast.mcaf_library as mcaf
-from optparse import OptionParser
+import click
 
-cmdline = OptionParser()
-cmdline.add_option('-m', '--mode', dest="trigger_mode",
-        action="store", default="intent",
-        help="Trigger on what field? (modes currently accpeted: intent, project). [DEFAULT: intent]")
-cmdline.add_option('-t', '--value', dest="trigger_value",
-        action="store", default="realfast",
-        help="Triggers if trigger field contains this string. [DEFAULT: realfast]")
-cmdline.add_option('-l', '--listen', dest="listen",
-        action="store_true", default=False,
-        help="Only listen to multicast, don't launch anything") 
-cmdline.add_option('-v', '--verbose', dest="verbose",
-        action="store_true", default=False,
-        help="More verbose output")
-(opt,args) = cmdline.parse_args()
-
-progname = 'main_controller'
-
-# Set up verbosity level for log
-loglevel = logging.INFO
-if opt.verbose:
-    loglevel = logging.DEBUG
-
-logging.basicConfig(format="%(asctime)-15s %(levelname)8s %(message)s",
-        level=loglevel)
-
-logging.info('%s started' % progname)
-
-logging.info("Trigger mode %s; will trigger on value \"%s\"" % (opt.trigger_mode,opt.trigger_value))
-
-if opt.listen:
-    logging.info('Running in listen-only mode')
-
-node = os.uname()[1]
-
+logging.basicConfig(format="%(asctime)-15s %(levelname)8s %(message)s", level=logging.INFO)
+        
+mode_default = "intent"
+value_default= "realfast"
+listen_default = True
+progname_default = "main_controller"
 
 class FRBController(object):
     """Listens for OBS packets and tells FRB processing about any
     notable scans."""
 
-    def __init__(self,mode="project",):
+    def __init__(self, trigger_mode=mode_default, trigger_value=value_default, listen=listen_default, mode="project"):
         # Mode can be project, intent
-        self.trigger_mode = opt.trigger_mode
-        self.trigger_value = opt.trigger_value
+        self.trigger_mode = trigger_mode
+        self.trigger_value = trigger_value
         self.dotrigger = False
 
     def add_sdminfo(self,sdminfo):
@@ -79,14 +51,35 @@ class FRBController(object):
             # If we're not in listening mode, submit the pipeline for this scan as a queue submission.
             job = ['queue_rtpipe.py', config.sdmLocation, '--scans', str(config.scan), '--mode', 'rtsearch', '--paramfile', 'rtparams.py']
             logging.info("Ready to submit scan %d as job %s" % (config.scan, ' '.join(job)))
-            if not opt.listen:
+            if not listen:
                 logging.info("Submitting scan %d as job %s" % (config.scan, ' '.join(job)))
                 subprocess.call(job)
         else:
             logging.info("Received %s: %s" % (self.trigger_mode,compString)) 
             logging.info("Its BDF is in %s\n" % (config.bdfLocation)) 
 
-if __name__ == '__main__':
+@click.command()
+@click.option('--progname', default=progname_default, help='Program name used to trigger action')
+@click.option('--trigger_mode', '-m', default=mode_default, help="Trigger on what field? (modes currently accpeted: intent, project). [DEFAULT: intent]")
+@click.option('--trigger_value', '-t', default=value_default, help="Triggers if trigger field contains this string. [DEFAULT: realfast]")
+@click.option('--listen', '-l', default=listen_default, help="Only listen to multicast, don't launch anything", is_flag=True )
+@click.option('--verbose', '-v', default=False, help="More verbose output", is_flag=True)
+def monitor(progname, trigger_mode, trigger_value, listen, verbose):
+    """ Monitor of mcaf observation files. Blocking function.
+    """
+
+    logging.info('%s started' % progname)
+    logging.info("Trigger mode %s; will trigger on value \"%s\"" % (trigger_mode, trigger_value))
+
+    # Set up verbosity level for log
+    if verbose:
+        logger = logging.getLogger()
+        logger.setLevel(logging.DEBUG)
+        logging.debug('Running in verbose mod')
+
+    if listen:
+        logging.info('Running in listen-only mode')
+
     # This starts the receiving/handling loop
     controller = FRBController()
     sdminfo_client = mcaf.SdminfoClient(controller)
