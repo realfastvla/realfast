3a973d553a0cff18e6949482efc9bb9dfc7b59a4
Casey Law <caseyjlaw@gmail.com>
2016-01-22 17:56:53 -0700

    untested jinja2 templating to make index.html for cand summary plots

--- realfast/rtutils.py
+++ realfast/rtutils.py
@@ -10,6 +10,10 @@ import rtpipe.candvis as cv
 import cPickle as pickle
 import os, glob, time, shutil, subprocess, logging
 import sdmreader
+try:
+    from jinja2 import Environment
+except ImportError:
+    print('Jinja2 not available')
 
 default_bdfdir = '/lustre/evla/wcbe/data/no_archive'
 logger = logging.getLogger(__name__)
@@ -601,3 +605,37 @@ def sdmasorig(filename):
     shutil.move(os.path.join(filename, 'Main.xml'), os.path.join(filename, 'Main_cal.xml'))
     shutil.move(os.path.join(filename, 'Main_orig.xml'), os.path.join(filename, 'Main.xml'))
     shutil.move(os.path.join(filename, 'ASDMBinary'), os.path.join(filename, 'ASDMBinary_cal'))
+
+
+# stuff for jinja2 template for index to browse cand summary plots
+
+HTML = """
+<html>
+<body>
+
+ <ul>
+ {% for item in contents %}
+  <li><a href="{{ item[1] }}" target="frame">{{ item[0] }}</a> ({{ item[2] }} MB)</li>
+ {% endfor %}
+ </ul>
+
+</body>
+</html>
+"""
+
+def gethtmlcontents(directory):
+    filenames = glob.glob(os.path.join(directory, 'cands*_merge.html'))
+    mjds = ['.'.join(ff.rstrip('_merge.html').split('.')[-2:])
+            for ff in filenames]
+    sizes = [os.stat(ff).st_size/(1024**2)
+             for ff in filenames]
+    contents = zip(mjds, filenames, sizes)
+    return sorted(contents, key=lambda ff: ff[0])
+
+
+def writehtml(directory, outname='contents.html'):
+    contents = gethtmlcontents(directory)
+    contentshtml = Environment().from_string(HTML).render(contents=contents, title='List of Contents')
+
+    with open(outname, 'w') as f:
+        f.write(contentshtml)
