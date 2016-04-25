0c0553cf52705eaf38f5b496189963c6e7e78f66
Sarah Burke Spolaor <sarahbspolaor@gmail.com>
2016-04-25 11:03:41 -0600

    Included nrao_controls_archiving input option, which allows us to run parallel to a normal NRAO archiving data flow. MCAF now makes hardlinks to no_archive from bunker. I'm about to propagate the necessary changes to queue_monitor to complete this capability.

--- realfast/mcaf_monitor.py
+++ realfast/mcaf_monitor.py
@@ -56,6 +56,7 @@ import asyncore
 import click
 import logging
 import sdmreader 
+import 
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
