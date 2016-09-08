26105c16747c76f7bb1517fa015f752d68fc0486
Casey Law <caseyjlaw@gmail.com>
2016-09-08 15:15:53 -0600

    version bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description = 'Python scripts for running real-time data analysis at the VLA',
     author = 'Casey Law + SBS',
     author_email = 'caseyjlaw@gmail.com',
-    version = '1.28',
+    version = '1.29',
     url = 'http://github.com/caseyjlaw/realfast',
     include_package_data=True,
     packages = find_packages(),
