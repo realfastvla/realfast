3e4ada425701dbb986762bb3b44dd243443c7eb9
Casey Law <caseyjlaw@gmail.com>
2017-05-25 15:03:58 -0600

    controller can use 1segment pipeine

--- realfast/rfcontroller.py
+++ realfast/rfcontroller.py
@@ -9,13 +9,20 @@ class realfast_controller(Controller):
         super(realfast_controller, self).__init__()
         self.preffile = preffile
         self.inprefs = inprefs
+        self.fut = []
 
     def handle_config(self, config):
         """ Triggered when obs comes in.
         Downstream logic starts here.
         """
 
+        logging.info('ingesting config metadata to rfpipe')
         meta = rfpipe.metadata.config_metadata(config)
-        logging.info('config metadata ingested by rfpipe'.format(meta))
+
+        logging.info('generating rfpipe state')
         st = rfpipe.state.State(preffile=self.preffile, inmeta=meta, inprefs=self.inprefs)
-        logging.info('rfpipe state generated'.format(meta))
+
+        fut = rfpipe.pipeline.pipeline_vys2(st, 0)
+        logging.info('started pipeline')
+
+        self.futures.append(fut)
