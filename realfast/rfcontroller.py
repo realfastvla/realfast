b75b930676b624a322b70b6316361d8f574ca3af
Casey Law <caseyjlaw@gmail.com>
2017-06-13 15:26:16 -0600

    pulling scantime out since scan config now defines ent time

--- realfast/rfcontroller.py
+++ realfast/rfcontroller.py
@@ -7,21 +7,20 @@ logger = logging.getLogger(__name__)
 distributed_host = 'cbe-node-01'
 vys_cfile = '/home/cbe-master/realfast/soft/vysmaw_apps/vys.conf'
 default_preffile = '/lustre/evla/test/realfast/realfast.yml'
-default_scantime = 60 # in seconds
+#default_scantime = 60 # in seconds
 default_vys_timeout = 10 # in seconds
 
 class realfast_controller(Controller):
 
-    def __init__(self, scantime=default_scantime, preffile=default_preffile, inprefs={}, vys_timeout=default_vys_timeout):
+    def __init__(self, preffile=default_preffile, inprefs={}, vys_timeout=default_vys_timeout):
         """ Creates controller object that can act on a scan configuration.
         Inherits a "run" method that starts asynchronous operation.
-        scantime length in seconds to try to catch data with vysmaw.
         """
 
         super(realfast_controller, self).__init__()
         self.preffile = preffile
         self.inprefs = inprefs
-        self.scantime = scantime
+#        self.scantime = scantime
         self.vys_timeout = vys_timeout
 
     def handle_config(self, config):
@@ -33,14 +32,14 @@ class realfast_controller(Controller):
         if config.is_complete():
             # calc nints (this can be removed once scan config includes scan end time)
             try:
-                subband0 = config.get_subbands()[0]
-                inttime = subband0.hw_time_res  # assumes that vys stream comes after hw integration
-                nints = int(self.scantime/inttime)
-                logger.info('Pipeline will be set to catch {0} s of data ({1} integrations)'.format(self.scantime, nints))
-                inmeta = {'nints': nints}
+#                subband0 = config.get_subbands()[0]
+#                inttime = subband0.hw_time_res  # assumes that vys stream comes after hw integration
+#                nints = int(self.scantime/inttime)
+#                logger.info('Pipeline will be set to catch {0} s of data ({1} integrations)'.format(self.scantime, nints))
+#                inmeta = {'nints': nints}
 
                 logger.info('Generating rfpipe state...')
-                st = rfpipe.state.State(config=config, preffile=self.preffile, inprefs=self.inprefs, inmeta=inmeta)
+                st = rfpipe.state.State(config=config, preffile=self.preffile, inprefs=self.inprefs) #, inmeta=inmeta)
             
                 logger.info('Starting pipeline...')
                 fut = rfpipe.pipeline.pipeline_scan(st, host=distributed_host, cfile=vys_cfile, vys_timeout=self.vys_timeout)
