8b868b5ab349f8b92dda33191723411d06775c31
Casey Law <caseyjlaw@gmail.com>
2017-07-27 15:36:10 -0600

    segments as input

--- realfast/rfcontroller.py
+++ realfast/rfcontroller.py
@@ -1,19 +1,26 @@
+from __future__ import print_function, division, absolute_import #, unicode_literals # not casa compatible
+from builtins import bytes, dict, object, range, map, input#, str # not casa compatible
+from future.utils import itervalues, viewitems, iteritems, listvalues, listitems
+from io import open
+
 from evla_mcast.controller import Controller
 import rfpipe
+import distributed
 
 import logging
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger('rfpipe')
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger()
 
-distributed_host = 'cbe-node-01'
 vys_cfile = '/home/cbe-master/realfast/soft/vysmaw_apps/vys.conf'
 default_preffile = '/lustre/evla/test/realfast/realfast.yml'
-#default_scantime = 60 # in seconds
-default_vys_timeout = 10 # in seconds
+default_vys_timeout = 10  # seconds more than segment length
+distributed_host = 'cbe-node-01'
+
 
 class realfast_controller(Controller):
 
-    def __init__(self, preffile=default_preffile, inprefs={}, vys_timeout=default_vys_timeout):
+    def __init__(self, preffile=default_preffile, inprefs={},
+                 vys_timeout=default_vys_timeout):
         """ Creates controller object that can act on a scan configuration.
         Inherits a "run" method that starts asynchronous operation.
         """
@@ -21,7 +28,6 @@ class realfast_controller(Controller):
         super(realfast_controller, self).__init__()
         self.preffile = preffile
         self.inprefs = inprefs
-#        self.scantime = scantime
         self.vys_timeout = vys_timeout
 
     def handle_config(self, config):
@@ -29,12 +35,26 @@ class realfast_controller(Controller):
         Downstream logic starts here.
         """
 
-        logger.info('Docs received (ant, vci, obs): {0}, {1}, {2}'.format(config.ant, config.vci, config.obs))
+        logger.info('Received complete configuration for {0}, scan {1}'
+                    .format(config.scanId, config.scanNo))
+
+        logger.info('TODO: print more config info')
+
         try:
             logger.info('Generating rfpipe state...')
-            st = rfpipe.state.State(config=config, preffile=self.preffile, inprefs=self.inprefs) #, inmeta=inmeta)
-            
+            st = rfpipe.state.State(config=config, preffile=self.preffile,
+                                    inprefs=self.inprefs)
+
             logger.info('Starting pipeline...')
-            fut = rfpipe.pipeline.pipeline_scan(st, host=distributed_host, cfile=vys_cfile, vys_timeout=self.vys_timeout)
+            rfpipe.pipeline.pipeline_scan_distributed(st, segments=[0]
+                                                      host=distributed_host,
+                                                      cfile=vys_cfile,
+                                                      vys_timeout=self.vys_timeout)
         except KeyError as exc:
             logger.warn('KeyError in parsing VCI? {0}'.format(exc))
+
+    def handle_finish(self, dataset):
+        """ Triggered when obs doc defines end of a script.
+        """
+
+        logger.info('End of scheduling block message received')
