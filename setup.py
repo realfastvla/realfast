1b39ebe4dc7fc6346727c506de31ded28f0237e0
Casey Law <caseyjlaw@gmail.com>
2017-12-01 16:06:35 -0700

    moved pycuda out of default build requirement

--- setup.py
+++ setup.py
@@ -14,8 +14,8 @@ setup(
     data_files=[('conf', glob.glob('conf/*'))],
 #    scripts=['scripts/sdm_chop-n-serve.pl'],   # add non-python scripts
     install_requires=['rfpipe', 'evla_mcast', 'sdmpy', 'click',
-                      'elasticsearch', 'distributed', 'pyfft', 'pycuda'],
-                      #  'vysmaw_reader'],
+                      'elasticsearch', 'distributed', 'pyfft'],
+                      #  'pycuda', 'vysmaw_reader'],
     entry_points='''
         [console_scripts]
         realfast=realfast.cli:cli
