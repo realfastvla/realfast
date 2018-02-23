876d013111db26eb68d2e53a4150f5fff7055b4c
Casey J Law <caseyjlaw@gmail.com>
2018-02-23 08:36:35 -0800

    futures -> future

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     data_files=[('conf', glob.glob('conf/*'))],
 #    scripts=['scripts/sdm_chop-n-serve.pl'],   # add non-python scripts
     install_requires=['rfpipe', 'evla_mcast', 'sdmpy', 'click',
-                      'elasticsearch', 'distributed', 'pyfft', 'futures'],
+                      'elasticsearch', 'distributed', 'pyfft', 'future'],
                       # 'rfgpu', 'vysmaw_reader'],
     entry_points='''
         [console_scripts]
