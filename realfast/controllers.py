91083ae3733165a2eb4639aa06b6f01223a9faea
Casey Law <caseyjlaw@gmail.com>
2019-09-06 15:00:24 -0600

    not using client.run for logging

--- realfast/controllers.py
+++ realfast/controllers.py
@@ -650,9 +650,10 @@ class realfast_controller(Controller):
         # run memory logging
         memory_summary = ','.join(['({0}, {1})'.format(v['id'], v['metrics']['memory']/1e9) for k, v in iteritems(self.client.scheduler_info()['workers']) if v['metrics']['memory']/1e9 > 10])
         if memory_summary:
-            workers_highmem = [k for k, v in iteritems(self.client.scheduler_info()['workers']) if v['metrics']['memory']/1e9 > 10]
-            self.client.run(logging_statement, memory_summary, workers=workers_highmem)
             logger.info("High memory usage on cluster: {0}".format(memory_summary))
+            workers_highmem = [k for k, v in iteritems(self.client.scheduler_info()['workers']) if v['metrics']['memory']/1e9 > 10]
+            for w in workers_highmem:
+                distributed.fire_and_forget(self.client.submit(logging_statement, memory_summary, workers=workers_highmem, pure=False))
 
         # clean futures and get finished jobs
         removed = self.removefutures(badstatuslist)
