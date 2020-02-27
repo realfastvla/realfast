bd77ad58cafec3e3fffaff8f47c22ce1943554b3
Casey Law <caseyjlaw@gmail.com>
2020-02-27 15:01:06 -0700

    memory limit bump

--- realfast/controllers.py
+++ realfast/controllers.py
@@ -688,10 +688,10 @@ class realfast_controller(Controller):
                                 ','.join(scanIds)))
 
         # run memory logging
-        memory_summary = ','.join(['({0}, {1})'.format(v['id'], v['metrics']['memory']/1e9) for k, v in iteritems(self.client.scheduler_info()['workers']) if v['metrics']['memory']/1e9 > 10])
+        memory_summary = ','.join(['({0}, {1})'.format(v['id'], v['metrics']['memory']/1e9) for k, v in iteritems(self.client.scheduler_info()['workers']) if v['metrics']['memory']/1e9 > 14])
         if memory_summary:
             logger.info("High memory usage on cluster: {0}".format(memory_summary))
-            workers_highmem = [k for k, v in iteritems(self.client.scheduler_info()['workers']) if v['metrics']['memory']/1e9 > 12]
+            workers_highmem = [k for k, v in iteritems(self.client.scheduler_info()['workers']) if v['metrics']['memory']/1e9 > 14]
             for w in workers_highmem:
                 distributed.fire_and_forget(self.client.submit(logging_statement, memory_summary, workers=workers_highmem, pure=False))
 
