3a6780538bb56e273eb7111723452a1fb62e1fcc
Casey Law <caseyjlaw@gmail.com>
2024-07-09 11:28:09 -0600

    safer sdm generation and transfer

--- scripts/rfarchive.sh
+++ scripts/rfarchive.sh
@@ -5,7 +5,7 @@ SDMNAME=$2
 if [ $MODE == "create" ]; then
     ssh rfr "conda activate development; realfast buildsdm --indexprefix final --copybdf --sdmname $SDMNAME; rsync -rL --remove-source-files ${SDMNAME} claw@nmpost-master:~/fasttransients/realfast/tmp ; find ${SDMNAME} -type d -empty -delete"
 elif [ $MODE == "ingest" ]; then
-    ssh nmngas "activate_profile dsoc-test; realfastIngest -s /lustre/aoc/projects/fasttransients/realfast/tmp -p /lustre/aoc/projects/fasttransients/realfast/plots/final ${SDMNAME}"
+    ssh vlapipe@nmngas "activate_profile dsoc-test; realfastIngest -s /lustre/aoc/projects/fasttransients/realfast/tmp -p /lustre/aoc/projects/fasttransients/realfast/plots/final ${SDMNAME}"
 else
     echo "Oops"
 fi
